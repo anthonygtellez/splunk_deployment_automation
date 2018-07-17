b5684cbb548b3f11fb396310a789c8237ca1e7f7
Anthony Tellez <anthonygtellez@gmail.com>
2018-07-17 18:50:50 -0400

    updated
    updated with local configuration

--- upgrade/splunk-uf/local.sh
+++ upgrade/splunk-uf/local.sh
@@ -2,8 +2,7 @@
 #
 # ===========================================================
 # Purpose:
-# Parameters:	${1} =
-#               ${2} =
+# Parameters:	${1} = path to splunkforwarder install
 # Example usage: $ bash splunkinstall.sh splunk-6.3.2-aaff59bb082c-Linux-x86_64.tgz
 #
 # Privileges:	Must be run as root
@@ -15,15 +14,6 @@
 # Revision:	Last change: XX/XX/2017 by AT ::
 # ===========================================================
 #
-stopSplunk="sudo su - splunk -c '/opt/splunkforwarder/bin/splunk stop'"
-untarSplunk="tar -zxvf /tmp/${1} -C /opt && chown -R splunk:splunk /opt/splunkforwarder"
-startSplunk="sudo su - splunk -c '/opt/opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt'"
-for HOST in $(< $2); do
-    scp -r "${1}" $HOST:/tmp
-    ssh $HOST "$stopSplunk"
-    ssh $HOST "$untarSplunk"
-    ssh $HOST "$startSplunk"
-	if [ $? -ne 0 ]; then
-	  	echo "---- COULD NOT CONNECT TO $HOST ----"
-	fi
-done
+sudo su - splunk -c '/opt/splunkforwarder/bin/splunk stop'
+tar -zxvf ${1} -C /opt && chown -R splunk:splunk /opt/splunkforwarder
+sudo su - splunk -c '/opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt'
